f77805dba33e33b4c75d4b25868d571af168a566
Aleksey0x <alekseyostapchenko@gmail.com>
2025-05-29 15:38:45 +0300

    Refactor index.html and types.html to implement a slide-out navigation menu, enhancing user experience. Update package.json to include 'slideout' dependency for menu functionality. Remove unused CSS and JS files to optimize project structure.

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,35 @@
+import Slideout from 'slideout';
 // import gsap from 'gsap';
 // import { ScrollTrigger } from 'gsap/ScrollTrigger';
 // import { ScrollSmoother } from 'gsap/ScrollSmoother';
 // import { TextPlugin } from 'gsap/TextPlugin';
 import '../scss/main.scss';
 
+function initializeSlideout() {
+	const panelElement = document.getElementById('panel');
+	const menuElement = document.getElementById('menu');
+	const menuButtonElement = document.getElementById('menu-button');
+
+	if (panelElement && menuElement && menuButtonElement) {
+		const slideout = new Slideout({
+			panel: panelElement,
+			menu: menuElement,
+			padding: 256, // ширина меню
+			tolerance: 70 // чувствительность свайпа
+		});
+
+		// Кнопка открытия/закрытия
+		menuButtonElement.addEventListener('click', function () {
+			slideout.toggle();
+		});
+
+		console.log('Slideout инициализирован.');
+	} else {
+		console.log('Элементы для Slideout не найдены. Инициализация пропущена.');
+	}
+}
+
 document.addEventListener('DOMContentLoaded', function () {
+	initializeSlideout();
 	console.log('js работает');
 });
