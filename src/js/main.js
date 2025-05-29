a2007409a8f049d3cf040befe9ca7707324a4dc3
Aleksey0x <alekseyostapchenko@gmail.com>
2025-05-29 10:34:30 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+// import gsap from 'gsap';
+// import { ScrollTrigger } from 'gsap/ScrollTrigger';
+// import { ScrollSmoother } from 'gsap/ScrollSmoother';
+// import { TextPlugin } from 'gsap/TextPlugin';
+import '../scss/main.scss';
+
+document.addEventListener('DOMContentLoaded', function () {
+	console.log('js работает');
+});
