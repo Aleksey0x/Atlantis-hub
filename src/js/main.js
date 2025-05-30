0c1680ea9cd25813c55a2b96696657306fe5ca6a
AlekseyOs <alekseyostapchenko@gmail.com>
2025-05-30 08:23:21 +0300

    Implement transition effects for page navigation using GSAP in main.js. Update styles in main.scss for the transition frame and adjust HTML structure in index.html and types.html for improved layout and accessibility.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 import Slideout from 'slideout';
-// import gsap from 'gsap';
+import gsap from 'gsap';
 // import { ScrollTrigger } from 'gsap/ScrollTrigger';
 // import { ScrollSmoother } from 'gsap/ScrollSmoother';
 // import { TextPlugin } from 'gsap/TextPlugin';
@@ -30,6 +30,37 @@ function initializeSlideout() {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+	const frame = document.getElementById("transition-frame");
+	const links = document.querySelectorAll(".transition-link");
+
+	// Уход со страницы
+	links.forEach(link => {
+		link.addEventListener("click", (e) => {
+			e.preventDefault();
+			const href = link.getAttribute("href");
+
+			gsap.to(frame, {
+				top: 0,
+				duration: 0.7,
+				ease: "power2.inOut",
+				onComplete: () => {
+					window.location.href = href;
+				}
+			});
+		});
+	});
+
+	// Появление новой страницы
+	window.addEventListener("DOMContentLoaded", () => {
+		gsap.fromTo(frame,
+			{ top: 0 },
+			{
+				top: "-100%",
+				duration: 0.7,
+				ease: "power2.inOut"
+			}
+		);
+	});
 	initializeSlideout();
 	console.log('js работает');
 });
